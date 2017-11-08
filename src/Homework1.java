3037593856a5f2827118e57a7216553d66fd5b0a
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-11-08 13:38:29 +0200

    Commit #2

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,5 +1,6 @@
 public class Homework1 {
     public static void main(String[] args) {
-        
+        System.out.println("Как я провел лето 2017");
+        System.out.println("\t Отлично!!!");
     }
 }
