0ce88284fc242afa846341cae54ca40da9284dd8
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-11-30 10:30:25 +0200

    Homework 4 Task 4

---
+++ src/Module_4/Homework4/Task_4.java
@@ -0,0 +1,35 @@
+package Module_4.Homework4;
+
+public class Task_4 {
+    public static void main(String[] args) {
+
+    }
+    static int getMax(int a,int b) {
+        if (a < b) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+    static double getMax(double a,double b) {
+        if (a < b) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+    static double getMax(int a,double b) {
+        if (a < b) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+    static double getMax(double a,int b) {
+        if (a < b) {
+            return b;
+        } else {
+            return a;
+        }
+    }
+}
