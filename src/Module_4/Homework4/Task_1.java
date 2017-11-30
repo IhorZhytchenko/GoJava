3bd482e55e8c9c07017e5cf635a458da59cbef00
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-11-30 10:20:47 +0200

    Homework 4 Task 1

---
+++ src/Module_4/Homework4/Task_1.java
@@ -0,0 +1,11 @@
+package Module_4.Homework4;
+
+public class Task_1 {
+    public static void main(String[] args) {
+
+    }
+
+    static void numbers (int x){
+        for (int i=1; i<=x;i++) System.out.println(i);
+    }
+}
