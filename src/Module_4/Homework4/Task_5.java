71494df436f9d9e8ad08b49a4321c6e4dd7a7f39
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-12-02 14:02:02 +0200

    Homework 4 Task 5

--- src/Module_4/Homework4/Task_5.java
+++ src/Module_4/Homework4/Task_5.java
@@ -2,14 +2,14 @@ package Module_4.Homework4;
 
 public class Task_5 {
     public static void main(String[] args) {
-numbers(10);
+
     }
-    static void numbers (int x){
+    static void Counting (int x){
         if (x==1){
             System.out.println(x);
         }
         else{
-            numbers(x-1);
+            Counting(x-1);
             System.out.println(x);
         }
     }
