9af51e004ee181d2f395bebaf061dc60005a6113
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-12-02 12:09:25 +0200

    Homework 4 Task 6

--- src/Module_4/Homework4/Task_6.java
+++ src/Module_4/Homework4/Task_6.java
@@ -2,27 +2,26 @@ package Module_4.Homework4;
 
 public class Task_6 {
     public static void main(String[] args) {
-        drawRectangle(6,6);
+        drawRectangle(3,4);
 
     }
     static void drawRectangle (int a, int b){
         if (b==1){
-            drawRec(a);
+            if (a == 1) {
+                System.out.println("+ ");
+
+            } else {
+                System.out.print("+ ");
+                drawRectangle(a - 1,b);
+
+            }
         }
         else {
-            drawRec(a);
-            System.out.println();
-            drawRectangle(a,b-1);
-        }
+            drawRectangle(a, b - 1);
+            drawRectangle(a,1);
 
-    }
-    static void drawRec (int a) {
-        if (a == 1) {
-            System.out.print("+ ");
-        } else {
-            drawRec(a - 1);
-            System.out.print("+ ");
-        }
 
+
+        }
     }
 }
