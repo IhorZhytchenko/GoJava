8cb96cf307382ed5b998b2213853022f4f59026e
IhorZhytchenko <igor.zhytchenko@gmail.com>
2017-12-08 10:30:08 +0200

    Class CarWheel

---
+++ src/Module_5/Homework5/MotorTransport/CarWheel.java
@@ -0,0 +1,27 @@
+package Module_5.Homework5.MotorTransport;
+
+public class CarWheel {
+    private double wheelCondition;
+
+    public CarWheel(double wheelCondition){
+        this.wheelCondition = wheelCondition;
+    }
+    public CarWheel(){
+        this(1.0);
+    }
+
+    public void newWheel(){
+        this.wheelCondition = 1.0;
+    }
+    public void wearWheel(int x){
+        this.wheelCondition -= (double)x/100.0;
+    }
+    public double getWheelCondition(){
+        return this.wheelCondition;
+    }
+    public void showCondition(){
+        System.out.println("Состояние шины - " + this.wheelCondition*100 +"%" );
+
+    }
+
+}
