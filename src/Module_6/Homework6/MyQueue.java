0a28a51e98b04fa996f145d76ba54d42337981a1
IhorZhytchenko <igor.zhytchenko@gmail.com>
2018-01-18 18:18:35 +0200

    MyQueue

---
+++ src/Module_6/Homework6/MyQueue.java
@@ -0,0 +1,107 @@
+package Module_6.Homework6;
+
+public class MyQueue<T> {
+    private int size = 0;
+    private Node<T> first;
+    private Node<T> last;
+
+
+    public void add(T value){
+        Node<T> l = last;
+        Node<T> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+    public void remove(int index){
+        Node<T> x = node(index);
+        final T element = x.item;
+        final Node<T> next = x.next;
+        final Node<T> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+    }
+    public T peek() {
+        return (first == null) ? null : first.item;
+    }
+
+    public T poll() {
+        return (first == null) ? null : unlink(first);
+    }
+
+
+
+    public void clear() {
+        for (Node<T> x = first; x != null; ) {
+            Node<T> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    private   Node<T> node(int index) {
+        if (index < (size /2)) {
+            Node<T> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<T> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+    private T unlink(Node<T> f) {
+        final T element = f.item;
+        final Node<T> next = f.next;
+        f.item = null;
+        f.next = null;
+        first = next;
+        if (next == null)
+            last = null;
+        else
+            next.prev = null;
+        size--;
+        return element;
+    }
+
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+}
