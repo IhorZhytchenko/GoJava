d3450edb64dfd01ed6f1dc09f35ccb49381805a5
IhorZhytchenko <igor.zhytchenko@gmail.com>
2018-01-18 18:18:19 +0200

    MyLinkedList

---
+++ src/Module_6/Homework6/MyLinkedList.java
@@ -0,0 +1,91 @@
+package Module_6.Homework6;
+
+
+
+public class MyLinkedList<T> {
+    private int size = 0;
+    private Node<T> first;
+    private Node<T> last;
+
+
+    public void add(T value){
+        final Node<T> l = last;
+        final Node<T> newNode = new Node<>(l, value, null);
+        last = newNode;
+        if (l == null)
+            first = newNode;
+        else
+            l.next = newNode;
+        size++;
+    }
+    public void remove(int index){
+        Node<T> x = node(index);
+        final T element = x.item;
+        final Node<T> next = x.next;
+        final Node<T> prev = x.prev;
+
+        if (prev == null) {
+            first = next;
+        } else {
+            prev.next = next;
+            x.prev = null;
+        }
+
+        if (next == null) {
+            last = prev;
+        } else {
+            next.prev = prev;
+            x.next = null;
+        }
+
+        x.item = null;
+        size--;
+    }
+
+    public T get(int index) {
+        return node(index).item;
+    }
+
+    public void clear() {
+        for (Node<T> x = first; x != null; ) {
+             Node<T> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    private   Node<T> node(int index) {
+        if (index < (size /2)) {
+            Node<T> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<T> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+    private static class Node<T> {
+        T item;
+        Node<T> next;
+        Node<T> prev;
+
+        Node(Node<T> prev, T element, Node<T> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+}
